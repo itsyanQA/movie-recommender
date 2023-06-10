ba6e0393ae6e47ff0cbaac612c9fe270138678aa
stron <yaneliyahu83@gmail.com>
2023-06-10 20:57:26 +0300

    Add api provider in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { moviesApi } from "./store/slices/movieApiSlice";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <ApiProvider api={moviesApi}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </ApiProvider>
   );
 }
 
