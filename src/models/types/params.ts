16a856a70bb7bdb24a6256d5b89e76afa5cf01e0
stron <yaneliyahu83@gmail.com>
2023-06-10 20:59:54 +0300

    Add params file for storing all endpoints parameters

---
+++ src/models/types/params.ts
@@ -0,0 +1,11 @@
+export type GetUpcomingMoviesParams = {
+  titleType?: string;
+  year?: number;
+  genre?: string;
+  info?: string;
+  endYear?: number;
+  limit?: number;
+  sort?: string;
+  page?: string;
+  startYear?: number;
+};
