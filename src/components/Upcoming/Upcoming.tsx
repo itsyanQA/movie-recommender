48aecc9ebb5cbcbc71c9fe896208f7d2ee281c29
Yan Eliyahu <yan@adom.technology>
2023-06-10 23:07:55 +0300

    Add upcoming section - wip

--- src/components/Upcoming/Upcoming.tsx
+++ src/components/Upcoming/Upcoming.tsx
@@ -1,10 +1,26 @@
-import react from "react"
-import { Movie } from "../../models/movie.model"
+import { Movie } from "../../models/movie.model";
+import styles from "./Upcoming.module.scss";
 
 type UpcomingProps = {
-    movieData: Movie
-}
+  upcomingMovies: Movie[];
+};
 
-export function Upcoming() {
-
+export function Upcoming({ upcomingMovies }: UpcomingProps) {
+  return (
+    <section className={styles.sectionContainer}>
+      <div className={styles.sectionContainerLabel}>Upcoming Movies</div>
+      <div className={styles.sectionContainerCard}>
+        {upcomingMovies?.map((upcomingMovie) => {
+          if (upcomingMovie?.primaryImage?.url) {
+            return (
+              <img
+                className={styles.sectionContainerUpcomingMovieImage}
+                src={upcomingMovie?.primaryImage?.url}
+              />
+            );
+          }
+        })}
+      </div>
+    </section>
+  );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,15 @@
 import { Header } from "../../components/Header/Header";
 import { useGetUpcomingMoviesQuery } from "../../store/slices/movieApiSlice";
 import { useEffect } from "react";
+import { Upcoming } from "../../components/Upcoming/Upcoming";
 
 export function HomePage() {
-  const {data: upcomingMovies} = useGetUpcomingMoviesQuery();
-  
-  useEffect(() => {
-    console.log(upcomingMovies);
-  }, [upcomingMovies]);
+  const { data: upcomingMovies } = useGetUpcomingMoviesQuery();
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Upcoming upcomingMovies={upcomingMovies?.results}/>
+    </>
+  );
 }
