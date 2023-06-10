90f0607b396d7118fefc4d2fa41d74803648d50e
stron <yaneliyahu83@gmail.com>
2023-06-10 20:56:47 +0300

    Add movie api slice

---
+++ src/store/slices/movieApiSlice.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetUpcomingMoviesParams } from "../../models/types/params";
+import { MovieEndpoints } from "../../models/enum/MovieEndpoints";
+
+export const moviesApi = createApi({
+  reducerPath: "moviesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://moviesdatabase.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", process.env.REACT_APP_API_KEY!);
+      headers.set("X-RapidAPI-Host", process.env.REACT_APP_API_HOST!);
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUpcomingMovies: builder.query<any, GetUpcomingMoviesParams | void>({
+      query: (args) => {
+        return { url: MovieEndpoints.UPCOMING_TITLES, params: { ...args } };
+      },
+    }),
+  }),
+});
+
+export const { useGetUpcomingMoviesQuery } = moviesApi;
