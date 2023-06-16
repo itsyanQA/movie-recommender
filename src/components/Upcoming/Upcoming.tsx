63c8bd96ee49bc1b0adff60f4f0eaa00745350c8
stron <yaneliyahu83@gmail.com>
2023-06-16 11:24:52 +0300

    Modify upcoming section to include error, loading and change card look

--- src/components/Upcoming/Upcoming.tsx
+++ src/components/Upcoming/Upcoming.tsx
@@ -1,22 +1,38 @@
 import { Movie } from "../../models/movie.model";
 import styles from "./Upcoming.module.scss";
+import { CircularLoading } from "../UI/CircularLoading";
+import { ErrorMessage } from "../../styled/ErrorMessage";
+import { FETCH_ERROR_TEXT } from "../../constants/Text";
 
 type UpcomingProps = {
   upcomingMovies: Movie[];
+  isLoading: boolean;
+  isError: boolean;
 };
 
-export function Upcoming({ upcomingMovies }: UpcomingProps) {
-  return (
+export function Upcoming({ upcomingMovies, isLoading, isError }: UpcomingProps) {
+  if (isError) {
+    return <ErrorMessage>{FETCH_ERROR_TEXT}</ErrorMessage>;
+  }
+
+  return isLoading ? (
+    <CircularLoading />
+  ) : (
     <section className={styles.sectionContainer}>
-      <div className={styles.sectionContainerLabel}>Upcoming Movies</div>
-      <div className={styles.sectionContainerCard}>
-        {upcomingMovies?.map((upcomingMovie) => {
+      <h2 className={styles.sectionContainerHeader}>Upcoming Movies</h2>
+      <div className={styles.sectionContainerRow}>
+        {upcomingMovies?.map((upcomingMovie: Movie, index: number) => {
           if (upcomingMovie?.primaryImage?.url) {
             return (
-              <img
-                className={styles.sectionContainerUpcomingMovieImage}
-                src={upcomingMovie?.primaryImage?.url}
-              />
+              <div className={styles.sectionContainerCard}>
+                <img
+                  key={index}
+                  alt="movie cover"
+                  className={styles.sectionContainerUpcomingMovieImage}
+                  src={upcomingMovie?.primaryImage?.url}
+                />
+                <p className={styles.sectionContainerMovieTitle}>{upcomingMovie?.originalTitleText.text}</p>
+              </div>
             );
           }
         })}
