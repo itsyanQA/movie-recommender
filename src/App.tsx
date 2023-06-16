74d31b8226abb7595b56162436d108db957f9d3c
stron <yaneliyahu83@gmail.com>
2023-06-16 11:21:54 +0300

    Change index and app to use sass instead of css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
