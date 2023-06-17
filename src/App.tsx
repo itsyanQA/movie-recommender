6fd53a9bf535b608e94965d947c95c3f81568cb7
stron <yaneliyahu83@gmail.com>
2023-06-17 19:05:45 +0300

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { moviesApi } from "./store/slices/movieApiSlice";
+import { theme } from "./style/theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
   return (
     <ApiProvider api={moviesApi}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </ThemeProvider>
     </ApiProvider>
   );
 }
