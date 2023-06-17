efd1c13831ae9d04ee11388220a313563838a732
stron <yaneliyahu83@gmail.com>
2023-06-17 20:16:32 +0300

    Update homepage to include upcoming section horizontally

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,29 @@
-// import { Header } from "../../components/Header/Header";
-// import { MovieCatalog } from "../../components/MovieCatalog/MovieCatalog";
-// import { useFetchMovies } from "../../hooks/useFetchMovies";
-// import { MovieEndpoints } from "../../models/enums/MovieEndpoints";
+import { Header } from "../../components/Header/Header";
+import { MovieCatalog } from "../../components/MovieCatalog/MovieCatalog";
+import { useFetchMovies } from "../../hooks/useFetchMovies";
+import { MovieEndpoints } from "../../models/enums/MovieEndpoints";
 
 export function HomePage() {
-  // const {
-  //   data: upcomingMovies,
-  //   isLoading,
-  //   isError,
-  //   isFetching,
-  //   page,
-  //   setPage,
-  // } = useFetchMovies(MovieEndpoints.UPCOMING_TITLES);
+  const RESPONSE_LIMIT = 20;
+  const {
+    data: upcomingMovies,
+    isLoading,
+    isError,
+    isFetching,
+    page,
+    setPage,
+  } = useFetchMovies(MovieEndpoints.UPCOMING_TITLES, RESPONSE_LIMIT);
 
-  return null // decide what to show in home page (probably snippets of upcoming, most popular etc...);
+  return (
+    <MovieCatalog
+      movies={upcomingMovies?.results}
+      isLoading={isLoading}
+      isError={isError}
+      isFetching={isFetching}
+      page={page}
+      setPage={setPage}
+      catalogHeaderText="Upcoming Movies"
+      shouldShowPagination={false}
+    />
+  );
 }
