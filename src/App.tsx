d753ff09814729ec5b14f31b3d95e5b8ba764f28
stron <yaneliyahu83@gmail.com>
2023-06-17 19:33:37 +0300

    Add upcoming page route

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { moviesApi } from "./store/slices/movieApiSlice";
 import { theme } from "./style/theme";
 import { ThemeProvider } from "@mui/material/styles";
+import { Header } from "./components/Header/Header";
+import { Upcoming } from "./pages/HomePage/Upcoming";
+import { RouterPaths } from "./models/enums/RouterPaths";
 
 function App() {
   return (
     <ApiProvider api={moviesApi}>
       <ThemeProvider theme={theme}>
+        <Header />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path={RouterPaths.HOME_PAGE} element={<HomePage />} />
+          <Route path={RouterPaths.UPCOMING} element={<Upcoming />} />
         </Routes>
       </ThemeProvider>
     </ApiProvider>
