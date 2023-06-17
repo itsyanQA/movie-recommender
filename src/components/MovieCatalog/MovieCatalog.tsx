281a82bc7d8a84b305f14d8853bdd8e4a577d2f6
stron <yaneliyahu83@gmail.com>
2023-06-17 16:18:14 +0300

    Now show movies with no image and show placeholder img instead

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -4,16 +4,36 @@ import { CircularLoading } from "../UI/CircularLoading/CircularLoading";
 import { ErrorMessage } from "../../styled/ErrorMessage";
 import { FETCH_ERROR_TEXT } from "../../constants/Text";
 import { Pagination } from "../UI/Pagination/Pagination";
+import LocalMoviesOutlinedIcon from "@mui/icons-material/LocalMoviesOutlined";
 
 type MovieCatalogProps = {
   movies: Movie[];
   isLoading: boolean;
   isError: boolean;
   catalogHeaderText: string;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export function MovieCatalog(props: MovieCatalogProps) {
-  const { movies, isLoading, isError, catalogHeaderText } = props;
+  const { movies, isLoading, isError, catalogHeaderText, setPage } = props;
+
+  const getMovieImage = (
+    movieCaption: string | undefined,
+    movieImageUrl: string | null
+  ): JSX.Element => {
+    if (movieImageUrl) {
+      return (
+        <img alt={movieCaption} className={styles.sectionContainerMovieImage} src={movieImageUrl} />
+      );
+    }
+    return (
+      <div
+        className={`${styles.sectionContainerMovieImage} ${styles.sectionContainerPlaceholderImage}`}
+      >
+        <LocalMoviesOutlinedIcon />
+      </div>
+    );
+  };
 
   if (isError) {
     return <ErrorMessage>{FETCH_ERROR_TEXT}</ErrorMessage>;
@@ -27,27 +47,19 @@ export function MovieCatalog(props: MovieCatalogProps) {
         <h2 className={styles.sectionContainerHeader}>{catalogHeaderText}</h2>
         <div className={styles.sectionContainerRow}>
           {movies?.map((movie: Movie, index: number) => {
-            if (movie?.primaryImage?.url) {
-              return (
-                <div className={styles.sectionContainerCard} key={index}>
-                  <img
-                    alt={movie?.primaryImage?.caption?.plainText}
-                    className={styles.sectionContainerUpcomingMovieImage}
-                    src={movie?.primaryImage?.url}
-                  />
-                  <p className={styles.sectionContainerMovieTitle}>
-                    {movie?.originalTitleText.text}
-                  </p>
-                  <p className={styles.sectionContainerMovieDate}>
-                    {`${movie?.releaseDate?.day}/${movie?.releaseDate?.month}/${movie?.releaseDate?.year}`}
-                  </p>
-                </div>
-              );
-            }
+            return (
+              <div className={styles.sectionContainerCard} key={index}>
+                {getMovieImage(movie?.primaryImage?.caption?.plainText, movie?.primaryImage?.url)}
+                <p className={styles.sectionContainerMovieTitle}>{movie?.originalTitleText.text}</p>
+                <p className={styles.sectionContainerMovieDate}>
+                  {`${movie?.releaseDate?.day}/${movie?.releaseDate?.month}/${movie?.releaseDate?.year}`}
+                </p>
+              </div>
+            );
           })}
         </div>
       </section>
-      <Pagination />
+      <Pagination setPage={setPage} />
     </>
   );
 }
