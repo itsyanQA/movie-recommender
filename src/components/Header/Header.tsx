960a6cd30e299231417ca1b8c9072fd060c42443
stron <yaneliyahu83@gmail.com>
2023-06-17 19:33:58 +0300

    Add upcoming category in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { RouterPaths } from "../../models/enums/RouterPaths";
 import { SearchBar } from "../SearchBar/SearchBar";
 import styles from "./Header.module.scss";
 import LocalMoviesOutlinedIcon from "@mui/icons-material/LocalMoviesOutlined";
@@ -8,7 +9,7 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.headerContainerHeadingWrapper} onClick={() => navigate("/")}>
+      <div className={styles.headerContainerHeadingWrapper} onClick={() => navigate(RouterPaths.HOME_PAGE)}>
         <h1>Movie Recommender</h1>
         <LocalMoviesOutlinedIcon fontSize="large" sx={{ color: "#daa520" }} />
       </div>
@@ -16,6 +17,12 @@ export function Header() {
         <ul>
           <li className={styles.headerContainerNavItem}>Geners</li>
           <li className={styles.headerContainerNavItem}>Actors</li>
+          <li
+            className={styles.headerContainerNavItem}
+            onClick={() => navigate(RouterPaths.UPCOMING)}
+          >
+            Upcoming
+          </li>
           <SearchBar />
         </ul>
       </nav>
