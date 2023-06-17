de8176cb836391037657ba88aff23e5dbf85a419
stron <yaneliyahu83@gmail.com>
2023-06-17 19:08:55 +0300

    Change error message color

--- src/styled/ErrorMessage.ts
+++ src/styled/ErrorMessage.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ErrorMessage = styled.p`
-  color: rgb(255, 0, 0);
+  color: #cc0000;
   text-align: center;
   font-weight: 900;
   font-size: 24px;
