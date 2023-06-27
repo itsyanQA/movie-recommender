c41fe8d8081eb9478936531cfeee0d267e63768c
stron <yaneliyahu83@gmail.com>
2023-06-27 23:28:24 +0300

    Add lazy loading attribute to img element that renders movies in catalog

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -47,6 +47,7 @@ export function MovieCatalog(props: MovieCatalogProps) {
           className={styles.sectionContainerMovieImage}
           src={movieImageUrl}
           onClick={() => onMovieClickHandler(movieId)}
+          loading="lazy"
         />
       );
     }
