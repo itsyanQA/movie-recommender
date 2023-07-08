d210720dbc259c25d4e963ddd23b47be4ab96c5f
stron <yaneliyahu83@gmail.com>
2023-07-08 18:29:42 +0300

    Add BaseInfoOptions enum

---
+++ src/models/enums/BaseInfoOptions.ts
@@ -0,0 +1,9 @@
+export enum BaseInfoOptions {
+  BASE_INFO = "base_info",
+  MINI_INFO = "mini_info",
+  CREATORS_DIRECTOS_WRITERS = "creators_directors_writers",
+  REVENUE_BUDGET = "revenue_budget",
+  EXTENDED_CAST = "extendedCast",
+  RATING = "rating",
+  AWARDS = "awards",
+}
