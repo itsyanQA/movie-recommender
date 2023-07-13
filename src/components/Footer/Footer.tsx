bc7c1253bc2b7aa70cf5cfb94c58354172638636
Yan Eliyahu <yan@adom.technology>
2023-07-13 20:31:32 +0300

    Add SocialLinks constants and use it in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styles from "./Footer.module.scss";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import * as SOCIAL_LINKS from "../../constants/SocialLinks";
 
 export function Footer() {
   // TODO: Do a reverse spin when in mouseLeave
@@ -9,12 +10,12 @@ export function Footer() {
       <LinkedInIcon
         className={styles.footerContainerSocialIcon}
         fontSize="large"
-        onClick={() => window.open(process.env.REACT_APP_LINKEDIN)}
+        onClick={() => window.open(SOCIAL_LINKS.LINKEDIN_PROFILE_URL)}
       />
       <GitHubIcon
         className={styles.footerContainerSocialIcon}
         fontSize="large"
-        onClick={() => window.open(process.env.REACT_APP_GITHUB)}
+        onClick={() => window.open(SOCIAL_LINKS.GITHUB_PROFILE_URL)}
       />
     </footer>
   );
---
+++ src/constants/SocialLinks.ts
@@ -0,0 +1,2 @@
+export const GITHUB_PROFILE_URL = "https://github.com/itsyanQA"
+export const LINKEDIN_PROFILE_URL = "https://linkedin.com/in/yan-eliyahu"
