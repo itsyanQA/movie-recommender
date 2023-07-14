ef1dd4a4ba478766568a0413391a419a3b1b92ed
Yan Eliyahu <yan@adom.technology>
2023-07-14 09:23:07 +0300

    Add movie fetch in Movie component

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -1,6 +1,18 @@
 import { useLocation } from "react-router-dom";
 import React from "react";
+import { useFetchMovies } from "../../hooks/useFetchMovies";
+import { MovieEndpoints } from "../../models/enums/MovieEndpoints";
+import { GetMovieByIdParams } from "../../models/types/params";
+import { BaseInfoOptions } from "../../models/enums/BaseInfoOptions";
 
 export function Movie() {
+  const location = useLocation();
+  const movieId: string | undefined = location.pathname.split("/").at(-1);
+  const movieByIdParam: GetMovieByIdParams = {
+    movieId: movieId as string,
+    info: BaseInfoOptions.BASE_INFO
+  };
+  const { data: movieData, isLoading, isError, isFetching } = useFetchMovies(MovieEndpoints.MOVIE, movieByIdParam);
+  console.log(movieData);
   return <h1>this is a movie</h1>;
 }
