bdb79b1c29d4422946ff668754abbae73e0dc80a
Yan Eliyahu <yan@adom.technology>
2023-07-14 12:24:56 +0300

    Add html layout for Movie component

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -1,18 +1,51 @@
+import styles from "./Movie.module.scss";
 import { useLocation } from "react-router-dom";
-import React from "react";
 import { useFetchMovies } from "../../hooks/useFetchMovies";
 import { MovieEndpoints } from "../../models/enums/MovieEndpoints";
-import { GetMovieByIdParams } from "../../models/types/params";
+import { GetMovieByIdParams } from "../../models/interfaces/params";
 import { BaseInfoOptions } from "../../models/enums/BaseInfoOptions";
+import { MovieByIdResponse } from "../../models/interfaces/base-movie-response.model";
+import { CircularLoading } from "../../components/UI/CircularLoading/CircularLoading";
+import { ErrorMessage } from "../../styled/ErrorMessage";
+import { FETCH_ERROR_TEXT } from "../../constants/Text";
+import { PlaceholderCard } from "../../components/UI/PlaceholderCard/PlaceholderCard";
 
 export function Movie() {
   const location = useLocation();
   const movieId: string | undefined = location.pathname.split("/").at(-1);
   const movieByIdParam: GetMovieByIdParams = {
     movieId: movieId as string,
-    info: BaseInfoOptions.BASE_INFO
+    info: BaseInfoOptions.BASE_INFO,
   };
-  const { data: movieData, isLoading, isError, isFetching } = useFetchMovies(MovieEndpoints.MOVIE, movieByIdParam);
-  console.log(movieData);
-  return <h1>this is a movie</h1>;
+  const { data, isLoading, isError, isFetching } = useFetchMovies(MovieEndpoints.MOVIE, movieByIdParam);
+  const movie: MovieByIdResponse = data?.results;
+  console.log(movie);
+
+  if (isLoading || isFetching) {
+    return <CircularLoading />;
+  } else if (isError) {
+    return <ErrorMessage>{FETCH_ERROR_TEXT}</ErrorMessage>;
+  }
+
+  return (
+    <div className={styles.movieInfo}>
+      {movie?.primaryImage?.url ? (
+        <img className={styles.movieInfoImage} src={movie?.primaryImage?.url} />
+      ) : (
+        <div className={styles.movieInfoImage} >
+          <PlaceholderCard dimension={{ height: "345px", width: "230px" }} />
+        </div>
+      )}
+      <div className={styles.movieInfoContent}>
+        <div className={styles.movieInfoContentTop}>
+          <span className={styles.movieInfoContentTopTitle}>{movie?.originalTitleText?.text}</span>
+          <span
+            className={styles.movieInfoContentTopDate}
+          >{`${movie?.releaseDate?.day}/${movie?.releaseDate?.month}/${movie?.releaseDate?.year}`}</span>
+          <span className={styles.movieInfoContentTopDirectedby}></span>
+        </div>
+        <p className={styles.movieInfoContentSummary}></p>
+      </div>
+    </div>
+  );
 }
