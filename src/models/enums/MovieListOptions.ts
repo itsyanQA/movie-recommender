a93bb65eeace397b1b6833f0b6c966f9f60ff99f
Yan Eliyahu <yaneliyahu83@gmail.com>
2023-07-29 13:25:45 +0300

    Add MovieListOptions enum

---
+++ src/models/enums/MovieListOptions.ts
@@ -0,0 +1,10 @@
+export enum MovieListOptions {
+  MOST_POP_MOVIES = "most_pop_movies",
+  MOST_POP_SERIES = "most_pop_series",
+  TOP_BOX_OFFICE = "top_boxoffice_200",
+  TOP_BOX_OFFICE_LAST_WEEKEND = "top_boxoffice_last_weekend_10",
+  TOP_RATED_250 = "top_rated_250",
+  TOP_RATED_ENGLISH_250 = "top_rated_english_250",
+  TOP_RATED_LOWEST_100 = "top_rated_lowest_100",
+  TOP_RATED_SERIES_250 = "top_rated_series_250",
+}
