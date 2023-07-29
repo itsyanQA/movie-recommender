9a3244f1411d69a7bbbb2978d5438f595e0b30cb
Yan Eliyahu <yaneliyahu83@gmail.com>
2023-07-29 13:27:09 +0300

    Add 'Top Rated' catalog in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,31 +2,56 @@ import { MovieCatalog } from "../../components/MovieCatalog/MovieCatalog";
 import { useFetchMovies } from "../../hooks/useFetchMovies";
 import { usePagination } from "../../hooks/usePagination";
 import { MovieEndpoints } from "../../models/enums/MovieEndpoints";
+import { MovieListOptions } from "../../models/enums/MovieListOptions";
+import { Sort } from "../../models/enums/Sort";
 import { TitleTypes } from "../../models/enums/TitleTypes";
 
 export function HomePage() {
-  const { page, setPage } = usePagination();
+  const LIMIT_PER_CATALOG = 20;
+
   const upcomingMovieParams = {
-    limit: 20,
+    limit: LIMIT_PER_CATALOG,
     titleType: TitleTypes.MOVIE,
   };
+  const mostPopularMoviesParams = {
+    limit: LIMIT_PER_CATALOG,
+    titleType: TitleTypes.MOVIE,
+    list: MovieListOptions.TOP_RATED_250,
+    sort: Sort.YEAR_DESCENDING,
+  };
+
   const {
     data: upcomingMovies,
-    isLoading,
-    isError,
-    isFetching,
+    isLoading: isUpcomingLoading,
+    isError: isUpcomingError,
+    isFetching: isUpcomingFetching,
   } = useFetchMovies(MovieEndpoints.UPCOMING_TITLES, upcomingMovieParams);
 
+  const {
+    data: topRated,
+    isLoading: isTopRatedLoading,
+    isError: isTopRatedError,
+    isFetching: isTopRatedFetching,
+  } = useFetchMovies(MovieEndpoints.MOVIE, mostPopularMoviesParams);
+
   return (
-    <MovieCatalog
-      movies={upcomingMovies?.results}
-      isLoading={isLoading}
-      isError={isError}
-      isFetching={isFetching}
-      page={page}
-      setPage={setPage}
-      catalogHeaderText="Upcoming Movies"
-      shouldShowPagination={false}
-    />
+    <>
+      <MovieCatalog
+        movies={topRated?.results}
+        isLoading={isTopRatedLoading}
+        isError={isTopRatedError}
+        isFetching={isTopRatedFetching}
+        catalogHeaderText="Top Rated"
+        shouldShowPagination={false}
+      />
+      <MovieCatalog
+        movies={upcomingMovies?.results}
+        isLoading={isUpcomingLoading}
+        isError={isUpcomingError}
+        isFetching={isUpcomingFetching}
+        catalogHeaderText="Upcoming Movies"
+        shouldShowPagination={false}
+      />
+    </>
   );
 }
