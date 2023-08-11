7bebe446d65e897f8411ad68ccf4e28875f3da62
stron <yaneliyahu83@gmail.com>
2023-08-11 11:52:00 +0300

    Move movie meta info to be separate component

---
+++ src/components/Movie/MovieMetaInfo/MovieMetaInfo.tsx
@@ -0,0 +1,44 @@
+import styles from "./MovieMetaInfo.module.scss";
+import { MovieByIdResponse } from "../../../models/interfaces/base-movie-response.model";
+import { ImdbLogo } from "../../../assets/imdb-logo";
+import StarIcon from "@mui/icons-material/Star";
+
+type MovieMetaInfoProps = {
+  movie: MovieByIdResponse;
+};
+
+export default function MovieMetaInfo({ movie }: MovieMetaInfoProps) {
+  return (
+    <div
+      className={styles.metaInfo}
+      style={{
+        borderBottom: movie?.plot?.plotText?.plainText ? "1px solid white" : "",
+      }}
+    >
+      <div className={styles.metaInfoFirstRow}>
+        <ImdbLogo movieId={movie.id} />
+        <span
+          className={styles.metaInfoDate}
+        >{`${movie?.releaseDate?.day}/${movie?.releaseDate?.month}/${movie?.releaseDate?.year}`}</span>
+        {movie?.ratingsSummary?.aggregateRating ? (
+          <div className={styles.metaInfoRating}>
+            <div className={styles.metaInfoRatingStarAndNumber}>
+              <StarIcon sx={{ color: "orange" }} fontSize="small" />
+              <span className={styles.metaInfoRatingNumber}>
+                {movie?.ratingsSummary?.aggregateRating}
+              </span>
+            </div>
+            {movie?.ratingsSummary?.voteCount ? (
+              <span className={styles.metaInfoRatingVotes}>
+                &nbsp;{`/ ${movie?.ratingsSummary?.voteCount} Votes`}
+              </span>
+            ) : null}
+          </div>
+        ) : null}
+      </div>
+      <span className={styles.metaInfoGenres}>
+        {movie?.genres?.genres?.map((genre) => genre.text).join("/")}
+      </span>
+    </div>
+  );
+}
