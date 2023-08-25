186cc16e187f78ea6352f01e3c4bc419508f4900
stron <yaneliyahu83@gmail.com>
2023-08-25 22:29:08 +0300

    Add show all/hide all cast button

--- src/components/Movie/MovieCast/MovieCast.tsx
+++ src/components/Movie/MovieCast/MovieCast.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import styles from "./MovieCast.module.scss";
 import CastCard from "./CastCard/CastCard";
 import { CastDetail } from "../../../models/interfaces/cast-detail.model";
+import { Button } from "@mui/material";
 
 type MovieCastProps = {
   cast: CastResponse;
@@ -10,6 +11,7 @@ type MovieCastProps = {
 
 export default function MovieCast({ cast }: MovieCastProps) {
   const [castDetails, setCastDetails] = useState<CastDetail[] | null>(null);
+  const [isShowAllCast, setIsShowAllCast] = useState<boolean>(false);
 
   // TODO: Remove the filteredDetails, and add an image placeholder if the url does not exist
   useEffect(() => {
@@ -26,14 +28,35 @@ export default function MovieCast({ cast }: MovieCastProps) {
     }
   }, [cast]);
 
-  return (
-    <>
-      <h2>Cast</h2>
+  const renderPartCast = (): JSX.Element => {
+    return (
+      <ul className={styles.castList} style={{ flexWrap: "nowrap" }}>
+        {castDetails?.map((castDetail, index) => {
+          if (index <= 4) {
+            return <CastCard castDetail={castDetail} />;
+          }
+        })}
+      </ul>
+    );
+  };
+
+  const renderFullCast = (): JSX.Element => {
+    return (
       <ul className={styles.castList}>
         {castDetails?.map((castDetail) => (
           <CastCard castDetail={castDetail} />
         ))}
       </ul>
-    </>
+    );
+  };
+
+  return (
+    <section className={styles.cast}>
+      <h2>Cast</h2>
+      {isShowAllCast ? renderFullCast() : renderPartCast()}
+      <Button className={styles.castShowAllButton} onClick={() => setIsShowAllCast((prevState) => !prevState)}>
+        {isShowAllCast ? "Hide Cast & Crew" : "Show All Cast & Crew"}
+      </Button>
+    </section>
   );
 }
