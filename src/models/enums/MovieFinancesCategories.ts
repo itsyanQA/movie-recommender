b862b645e4ba33107c262c9f5e866ee009b53598
stron <yaneliyahu83@gmail.com>
2023-08-26 15:16:25 +0300

    Create MovieFinancesCategories enum
    To be used in MovieAdditionalInfo component

---
+++ src/models/enums/MovieFinancesCategories.ts
@@ -0,0 +1,6 @@
+export enum MovieFinancesCategories {
+  LIFETIME_GROSS = "Lifetime Gross",
+  OPENING_WEEKEND_GROSS = "Opening Weekend Gross",
+  PRODUCTION_BUDGET = "Production Budget",
+  WORLD_WIDE_GROSS = "World Wide Gross",
+}
